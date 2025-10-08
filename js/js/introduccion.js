6bb4a67be641a111c1810f1990f3c82efe3e3c7a
Pamela Tarelo <pamelatarelo18@gmail.com>
2025-10-08 14:36:22 -0600

    Avance de los Simpson
    8 de octubre de 2025

--- js/js/introduccion.js
+++ js/js/introduccion.js
@@ -1,50 +1,25 @@
-let x = [1,2,3,4]
-const y=[20,50,...x]
-
-console.log(y)
-
-const edadLegalEEUU = 21
-const checkEstado = (edad, edadLegal)=>{
-    return edad >= edadLegal ? "Eres Mayor" : "Eres Menor"
-}
-
-const persona = checkEstado(18,edadLegalEEUU)
-console.log(persona)
-
-//JSON
-let arreglo = [
-    {id:1, name:"Luis", age:20},
-    {id:2, name:"Luis 3", age:22},
-    {id:3, name:"Luis 1", age:24},
-    {id:4, name:"Luis Test", age:25},
-]
-let ul = document.getElementById("lista")
-var todo =""
-arreglo.forEach((item)=>{
-    item.age *= 10
-    todo += 
-    `<li>${item.name} - edad ${item.age}</li>`
-})
-ul.innerHTML = todo
-
-console.log("arreglo original", arreglo);
-
-var copia = arreglo.map(item=>{
-    item.age *= 10
-    return item
-})
-console.log("arreglo modificado", copia);
+var ul = document.getElementById("lista")
 
 let url = "https://thesimpsonsapi.com/api/"
-fetch(url+"characters").then(response=>{
+
+let arr_original = []
+
+var lis = ""
+
+var getLista = (page)=>{
+fetch(url+"characters?page="+page).then(response=>{
     if(!response.ok){
         console.log("Error en la red")
     }
     return response.json()
-}).then(data=>{
+    }).then(data=>{
     console.log("Datos del Personaje: ", data)
-    var lis = ""
+    
     data.results.forEach(personaje=>{
+        arr_original.push({
+            personaje:personaje,
+            datos:null
+        })
         lis+=`
         <li>
             <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}"width="200px"/>
@@ -52,5 +27,9 @@ fetch(url+"characters").then(response=>{
         </li>`
     })
     ul.innerHTML = lis
+    console.log("Arreglo Final", arr_original)
 })
-
+}
+for(let i=1;i<=5;i++){
+    getLista(i)
+}
